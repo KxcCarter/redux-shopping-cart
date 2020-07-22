1b079423398e5b4e3b12fe6ddd25cf1d7899b79e
Kenneth Carter <kxccarter@gmail.com>
2020-07-22 15:24:53 -0500

    ProductForm and ProductList update to dispatch and display from productReducer.

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import ProductListItem from '../ProductListItem/ProductListItem';
 
 class ProductList extends Component {
-
-    render() {
-        return (
-            <div>
-                <ul>
-                   {this.props.products.map((product, i) => {
-                       return (
-                           <ProductListItem key={i} product={product} />
-                       );
-                   })} 
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.props.products.map((product, i) => {
+            return <ProductListItem key={i} product={product} />;
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default ProductList;
+const mapStoreToProps = (store) => {
+  return { products: store.productReducer };
+};
+
+export default connect(mapStoreToProps)(ProductList);
